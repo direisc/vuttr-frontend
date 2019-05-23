c443d863580fd7084db85bc0545040e77cea91a7
Diego Carvalho <direisc@gmail.com>
2019-05-22 21:22:45 -0300

    Heroku teste

--- src/App.vue
+++ src/App.vue
@@ -62,6 +62,7 @@ $modal-card-body-padding: 0 30px;
 @import "../node_modules/bulma/sass/utilities/_all";
 @import "../node_modules/bulma/sass/base/_all";
 @import "../node_modules/bulma/sass/elements/_all";
+@import "../node_modules/bulma/sass/form/_all";
 @import "../node_modules/bulma/sass/components/_all";
 @import "../node_modules/bulma/sass/grid/_all";
 @import "../node_modules/bulma/sass/layout/_all";
